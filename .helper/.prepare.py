e1c928cf056c63eb6349fc200f8f68ad23b12579
Andrew Dean <oneandydean@hotmail.com>
2017-02-05 10:28:40 +0000

    Update .prepare.py to work for the new relative links

--- .helper/.prepare.py
+++ .helper/.prepare.py
@@ -6,17 +6,15 @@ for item in os.listdir(os.getcwd()):
         old = open(item, "r")
         new = open(item + ".tmp", "w")
         for line in old:
-            line = line.replace(
-                "E:\\Users\\Andrew\\Desktop\\Websites\\hip-prop.co.uk\\working\\",
-                "http://hip-prop.co.uk/"
-                )
+            if not (("href=\"https://" in line) or ("href=\"#" in line)):
+                line = line.replace("href=\"", "href=\"/")
             new.write(line.replace(".html", "/"))
         old.close()
         new.close()
         os.remove(item)
         os.rename(item + ".tmp", item)
-                      
 
-for page in html:    
+
+for page in html:
     os.mkdir(page)
     os.rename(page + ".html", page + "/index.html")
