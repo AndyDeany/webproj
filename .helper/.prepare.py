ed296e53af99eb28ad97bcd983e4162f83b4f473
AndyDeany <oneandydean@hotmail.com>
2017-07-14 12:14:01 +0100

    Change .prepare.py to work with /de/ subfolder as well

--- .helper/.prepare.py
+++ .helper/.prepare.py
@@ -1,20 +1,29 @@
 import os
-html = []
-for item in os.listdir(os.getcwd()):
-    if item[-5:] == ".html" and item != "index.html":
-        html.append(item[:-5])
-        old = open(item, "r")
-        new = open(item + ".tmp", "w")
-        for line in old:
-            if not (("href=\"https://" in line) or ("href=\"http://" in line) or ("href=\"#" in line)):
-                line = line.replace("href=\"", "href=\"/")
-            new.write(line.replace(".html", "/"))
-        old.close()
-        new.close()
-        os.remove(item)
-        os.rename(item + ".tmp", item)
 
 
-for page in html:
-    os.mkdir(page)
-    os.rename(page + ".html", page + "/index.html")
+def convert_dirs():
+    html = []
+    for item in os.listdir(os.getcwd()):
+        if item[-5:] == ".html" and item != "index.html":
+            html.append(item[:-5])
+            old = open(item, "r")
+            new = open(item + ".tmp", "w")
+            for line in old:
+                if not (("href=\"https://" in line) or ("href=\"http://" in line) or ("href=\"#" in line)):
+                    line = line.replace("href=\"", "href=\"/")
+                new.write(line.replace(".html", "/"))
+            old.close()
+            new.close()
+            os.remove(item)
+            os.rename(item + ".tmp", item)
+
+
+    for page in html:
+        os.mkdir(page)
+        os.rename(page + ".html", page + "/index.html")
+
+convert_dirs()
+
+
+os.chdir("de")
+convert_dirs()
